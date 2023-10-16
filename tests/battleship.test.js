4c67dfeb00ba4809c61e7c0572c0029d5fc74136
Climax Mba <climaxium150@gmail.com>
2023-10-16 07:10:08 +0100

    Write Ship tests

---
+++ tests/battleship.test.js
@@ -0,0 +1,40 @@
+import { Ship } from "../src/battleship";
+
+describe("Ship", () => {
+  let ship1, ship2, ship3;
+
+  beforeEach(() => {
+    ship1 = new Ship(1);
+    ship2 = new Ship(2);
+    ship3 = new Ship(3);
+  });
+
+  test("accurate length property", () => {
+    expect(ship1.length).toBe(1);
+    expect(ship2.length).toBe(2);
+    expect(ship3.length).toBe(3);
+  });
+
+  test("increases hits on hit()", () => {
+    ship1.hit();
+    ship2.hit();
+    ship2.hit();
+
+    expect(ship1.hits).toBe(1);
+    expect(ship2.hits).toBe(2);
+    expect(ship3.hits).toBe(0);
+  });
+
+  test("sinks only when hits equals length", () => {
+    expect(ship1.isSunk() && ship2.isSunk() && ship3.isSunk()).toBeFalsy();
+    ship1.hit();
+    expect(ship1.isSunk()).toBeTruthy();
+    ship3.hit();
+    expect(ship3.isSunk()).toBeFalsy();
+    ship3.hit();
+    ship3.hit();
+    expect(ship3.isSunk()).toBeTruthy();
+    ship2.hit();
+    expect(ship2.isSunk()).toBeFalsy();
+  });
+});
