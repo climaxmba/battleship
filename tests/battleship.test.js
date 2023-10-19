755ed65740a3ee76d75d595637130e07a76a6811
Climax Mba <climaxium150@gmail.com>
2023-10-19 17:27:25 +0100

    Write Gameboard test

--- tests/battleship.test.js
+++ tests/battleship.test.js
@@ -1,4 +1,4 @@
-import { Ship } from "../src/battleship";
+import { GameBoard, Ship } from "../src/battleship";
 
 describe("Ship", () => {
   let ship1, ship2, ship3;
@@ -36,5 +36,50 @@ describe("Ship", () => {
     expect(ship3.isSunk()).toBeTruthy();
     ship2.hit();
     expect(ship2.isSunk()).toBeFalsy();
+  });
+});
+
+describe("GameBoard", () => {
+  let board1, board2;
+
+  beforeEach(() => {
+    board1, (board2 = new GameBoard());
+  });
+
+  test("no overlaping ships", () => {
+    const positionList1 = [],
+      positionList2 = [];
+    board1.ships.forEach((shipObj) => {
+      positionList1.push(...shipObj.coords);
+    });
+    board2.ships.forEach((shipObj) => {
+      positionList2.push(...shipObj.coords);
+    });
+
+    expect(positionList1.length === new Set(positionList1).size).toBeTruthy();
+    expect(positionList2.length === new Set(positionList2).size).toBeTruthy();
+  });
+
+  test("ships are not too close to each other", () => {
+    // Compare each position lists for each ship against each other
+    board1.ships.forEach((shipObj) => {
+      shipObj.coords.forEach((pos) => {
+        board1.ships.forEach((otherShipObj) => {
+          otherShipObj.coords.forEach(otherPos => {
+            expect(board1.getAdjSquares(pos).has(otherPos)).toBeFalsy();
+          });
+        });
+      });
+    });
+    
+    board2.ships.forEach((shipObj) => {
+      shipObj.coords.forEach((pos) => {
+        board2.ships.forEach((otherShipObj) => {
+          otherShipObj.coords.forEach(otherPos => {
+            expect(board2.getAdjSquares(pos).has(otherPos)).toBeFalsy();
+          });
+        });
+      });
+    });
   });
 });
