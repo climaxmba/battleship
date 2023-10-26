7adcc70d6eb62f5de95fb5e7fcfc70955336ba96
Climax Mba <climaxium150@gmail.com>
2023-10-26 16:56:58 +0100

    Add game loop

---
+++ src/index.js
@@ -0,0 +1,39 @@
+import pubSub from "./modules/pubsub";
+import { Player } from "./modules/battleship";
+
+(() => {
+  let player1 = new Player(""),
+    player2 = new Player("", true);
+
+  function setPlayer(areasList) {
+    areasList.forEach(player1.addShip.bind(player1));
+  }
+
+  function checkWin(p1 = player1, p2 = player2) {
+    if (p1.gameBoard.areAllSunk()) {
+      return p2;
+    } else if (p2.gameBoard.areAllSunk()) {
+      return p1;
+    }
+    return null;
+  }
+
+  async function gameLoop() {
+    const playQueue = [player1, player2];
+    while (true) {
+      const player = playQueue.shift(),
+        enemy = playQueue[0];
+      
+      const pos = await player.play(enemy.gameBoard);
+      enemy.gameBoard.receiveAttack(pos);
+
+      const winner = checkWin();
+      if (winner) {
+        // pubSub winner
+        return;
+      } else {
+        playQueue.push(player);
+      }
+    }
+  }
+})();
